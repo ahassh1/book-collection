72791ecafbcedbd52bf0bb569c22de558053a57c
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-12-31 20:39:14 +0600

    47-10 Implement sort functionality

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -7,20 +7,50 @@ import Book from "../Book/Book";
 
 const ReadList = () => {
   const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
   const data = useLoaderData();
 
   useEffect(() => {
     const storedBookData = getStoredBook();
-    const ConvertedStoredBooks = storedBookData.map((id) => parseInt(id));
+    const convertedStoredBooks = storedBookData.map((id) => parseInt(id));
+
     const myReadList = data.filter((book) =>
-      ConvertedStoredBooks.includes(book.bookId)
+      convertedStoredBooks.includes(book.bookId)
     );
 
     setReadList(myReadList);
-  }, []);
+  }, [data]);
+
+  const handleSort = (type) => {
+    setSort(type);
+
+    if (type === "pages") {
+      const sortedByPages = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages
+      );
+      setReadList(sortedByPages);
+    }
+
+    if (type === "rating") {
+      const sortedByRating = [...readList].sort((a, b) => b.rating - a.rating);
+      setReadList(sortedByRating);
+    }
+  };
 
   return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort by: {sort ? sort : "Select"}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <button onClick={() => handleSort("pages")}>Pages</button>
+          </li>
+          <li>
+            <button onClick={() => handleSort("rating")}>Rating</button>
+          </li>
+        </ul>
+      </details>
+
       <Tabs>
         <TabList>
           <Tab>Read List</Tab>
@@ -29,10 +59,11 @@ const ReadList = () => {
 
         <TabPanel>
           <h2>Book I Read</h2>
-          {readList.map((b) => (
-            <Book key={b.bookId} singleBook={b}></Book>
+          {readList.map((book) => (
+            <Book key={book.bookId} singleBook={book} />
           ))}
         </TabPanel>
+
         <TabPanel>
           <h2>My Wish List</h2>
         </TabPanel>
