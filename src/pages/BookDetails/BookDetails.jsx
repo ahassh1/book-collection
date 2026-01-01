3bbc981ad1a4f9ebd1a039ac1e017937d20d20d6
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-01-01 19:36:52 +0600

    sweetalert2 + react tostify + netlify deploy

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
 import { addToStoredDB } from "../utility/addToDB";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+import { ToastContainer, toast } from "react-toastify";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -14,12 +19,19 @@ const BookDetails = () => {
     // array or collection
     // if book already exist the show a alert
     // if book not exist then push in the collection or array
+    Swal.fire({
+      title: "Good job!",
+      text: "You clicked the button!",
+      icon: "success",
+    });
+    toast("Wow so easy!");
     addToStoredDB(id);
   };
   return (
     <div className="w-2/3 mx-auto m-6 bg-sky-50">
       <img className="md:w-48 w-39" src={image} alt="" />
       <h4>Book Name: {bookName}</h4>
+      <ToastContainer />
       <button
         onClick={() => handleMarkAsRead(id)}
         className="btn btn-info m-3 text-white"
